5fd08870fef41ab046ab1f58264a8a93135f9a97
西 悠之 <hiroyuki.nishi@motex.co.jp>
2020-11-28 16:29:21 +0900

    update angular11

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,8 @@
 import {NgModule} from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatInputModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 import {LoginComponent} from './login.component';
 
 @NgModule({
